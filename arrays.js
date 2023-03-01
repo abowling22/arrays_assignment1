220d4dcdef182f024804ebd031f431912a40b096
Drew Bowling <andrewwbowling@gmail.com>
2023-03-01 14:34:17 -0700

    add the array call methods to the code and comment out a brief explination above

--- arrays.js
+++ arrays.js
@@ -4,3 +4,13 @@ var ticket_Prices = ["1", "2", "5", "10"];
 
 var isSmelly = ["true", "false", "true", "true"];
 
+// This command is pushing the bronze variable at the end of the array
+color_Palette.push("bronze");
+
+// This command is adding the variables of 25 and 84 to beginning of the array
+ticket_Prices.unshift("25", "84");
+
+// This command is popping at the final variable in the "isSmelly" array
+isSmelly.pop()
+
+console.log(color_Palette, ticket_Prices, isSmelly);
